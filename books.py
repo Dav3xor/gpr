065113101c1f31638d981363afada72f480a63d6
David Case <Dav3xor@gmail.com>
2015-07-25 08:47:44 -0700

    started on argument parsing, cleanup.

--- books.py
+++ books.py
@@ -1,10 +1,21 @@
+import argparse
+
+arguments = {'--filter':  { 'help': 'string to find', 
+                            'type': str },
+             '--year':    { 'help': 'sort by year, ascending' },
+             '--reverse': { 'help': 'reverse sort order' } }
+              
 
 
+
+files = [ ['slash', '/', ['pubdate','first','last','title']],
+          ['csv',   ',', ['title','last','first','pubdate']],
+          ['pipe',  '|', ['first','last','title','pubdate']] ]
 
 
 def load_file(filename, delimiter, fields):
   def do_split(line):
-    return [field.strip() for field in line.split(delimiter)]
+    return (field.strip() for field in line.split(delimiter))
 
   # I hope this isn't too code-golf-y
   with open(filename, 'r') as infile:
@@ -17,15 +28,15 @@ def print_records(records):
 
 
 # using the list sort method, but sorted might be a better choice,
-# depending on what you're doing.
-
+# depending on what you're doing. sorted has more of the FP nature.
 def sort_records(records, field, descending=False):
   records.sort(key=lambda x:x['last'], reverse=descending)
- 
-records  = load_file('slash', '/', ['pubdate','first','last','title'])
-records += load_file('csv', ',', ['title','last','first','pubdate'])
-records += load_file('pipe', '|', ['first','last','title','pubdate'])
 
+
+
+records = []
+for file in files:
+  records += load_file(*file) 
 
 print_records(records)
 print "---"
