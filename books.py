107f4bd59b803742fa5d229597e720f995bbc0be
David Case <dave.case@hp.com>
2015-07-27 08:18:35 -0700

    tweaked a few things.

--- books.py
+++ books.py
@@ -7,18 +7,27 @@ Hello!
 I like to write a little discussion about my solution when
 finishing up a coding test like this.  First of all, this was
 a lot of fun!  I think you have designed a very good, pragmatic
-test.  
+test.  Honestly, I'd much rather do something like this than
+reverse a binary tree on a whiteboard.
 
 When I first looked at the problem, I thought "I could use the
-csv module, it allows for custom delimiters, etc...".  but
-then I thought a little more about it, and realized that 
-writing my own file handling would be a nice way to show off
-some functional composition skills.  The amount of code I
-actually wrote to handle file input is probably on a par
-with what the csv module would require, so I think it's ok.
+csv module, it allows for custom delimiters, etc...".  Instead,
+I rolled my own record loader.  The amount of actual code
+is almost the same, and it allows me to show off some nice
+functional composition things.
 
 In real life, on a deadline, I would probably just use the csv
 module and be done with it.
+
+Simplicity -- take a look at the tests, there are a few compromises
+made for simplicity.  If an exception is raised, I don't catch it
+and let Python display the task trace.  I'm assuming a technically
+proficient user who can figure out what's going on.  If the data
+is mismatched with the column definitions, it silently loads the
+records, and either drops a column, or ignores extra ones.
+
+Tests -- I think the tests are reasonably complete, given the size
+of the dataset.  
 
 Compact code is cool, but it can be difficult to read and 
 maintain.  I strived for brevity, but try to keep it readable.
@@ -33,9 +42,9 @@ David
 
 
 def print_records(records):
-  """print a list of book records."""
-  for record in records:
-    print "%(last)s, %(first)s, %(title)s, %(pubdate)s" % record
+    """print a list of book records."""
+    for record in records:
+        print "%(last)s, %(first)s, %(title)s, %(pubdate)s" % record
 
 # keep all the file info in one place, where it's easy to add
 # another file if you want.
@@ -48,16 +57,23 @@ files = [ ['slash', '/', ['pubdate','first','last','title']],
 
 # I couldn't help but notice the expected output of books.py was an exact match
 # with what the argparse library would output.  :)
-arguments = {'--filter':  { 'help': 'show a subset of books, looks for the argument as a substring of any of the fields', 
+arguments = {'--filter':  { 'help': 'show a subset of books, ' + \
+                                    'looks for the argument as a ' + \
+                                    'substring of any of the fields', 
                             'type': str },
-             '--year':    { 'help': 'sort the books by year, ascending instead of default sort',
+             '--year':    { 'help': 'sort the books by year, ascending ' + \
+                                    'instead of default sort',
                             'action': 'store_true' },
              '--reverse': { 'help': 'reverse sort', 
                             'action': 'store_true' } }
-              
-parser = argparse.ArgumentParser(description="Show a list of books, alphabetical ascending by author's last name")
+
+description="Show a list of books, alphabetical ascending by author's " + \
+            "last name"
+parser = argparse.ArgumentParser(description)
+
 for argument, options in arguments.iteritems():
-  parser.add_argument(argument, **options)
+    parser.add_argument(argument, **options)
+
 args = parser.parse_args()
 
 sort_key     = 'pubdate'        if args.year    else 'last'
@@ -67,7 +83,7 @@ reverse_sort = True             if args.reverse else False
 
 records = []
 for file in files:
-  records += load_file(*file, filter=args.filter) 
+    records += load_file(*file, filter=args.filter) 
 
 sort_records(records, sort_key, reverse_sort)
 print_records(records)
--- tests.py
+++ tests.py
@@ -4,94 +4,94 @@ from StringIO import StringIO
 from record_processing import load_file, sort_records
 
 class TestSortRecords(unittest.TestCase):
-  def test_normal_use(self):
-    records = [ {'a':1, 'b':2, 'c': 3},
-                {'a':4, 'b':1, 'c': 4} ]
-    sort_records(records, 'b')
-    self.assertEqual(records, [ {'a':4, 'b':1, 'c': 4},
-                                {'a':1, 'b':2, 'c': 3} ])
-  def test_reverse_order(self):
-    records = [ {'a':1, 'b':2, 'c': 3},
-                {'a':4, 'b':1, 'c': 4} ]
-    sort_records(records, 'a', True)
-    self.assertEqual(records, [ {'a':4, 'b':1, 'c': 4},
-                                {'a':1, 'b':2, 'c': 3} ])
-  def test_bad_key(self):
-    records = [ {'a':1, 'b':2, 'c': 3},
-                {'a':4, 'b':1, 'c': 4} ]
-    with self.assertRaises(KeyError):
-      sort_records(records, 'd', True)
+    def test_normal_use(self):
+        records = [ {'a':1, 'b':2, 'c': 3},
+                    {'a':4, 'b':1, 'c': 4} ]
+        sort_records(records, 'b')
+        self.assertEqual(records, [ {'a':4, 'b':1, 'c': 4},
+                                    {'a':1, 'b':2, 'c': 3} ])
+    def test_reverse_order(self):
+        records = [ {'a':1, 'b':2, 'c': 3},
+                    {'a':4, 'b':1, 'c': 4} ]
+        sort_records(records, 'a', True)
+        self.assertEqual(records, [ {'a':4, 'b':1, 'c': 4},
+                                    {'a':1, 'b':2, 'c': 3} ])
+    def test_bad_key(self):
+        records = [ {'a':1, 'b':2, 'c': 3},
+                    {'a':4, 'b':1, 'c': 4} ]
+        with self.assertRaises(KeyError):
+          sort_records(records, 'd', True)
   
    
 class TestLoadFile(unittest.TestCase):
-  # tests a normal, successful run.
-  def test_normal_use(self):
-    records = load_file('csv', ',', ['title','last','first','year'])
-    self.assertEqual(records, [{'year': '2008', 
-                                'first': 'Robert', 
-                                'last': 'Martin', 
-                                'title': 'Clean Code'}, 
-                               {'year': '2008', 
-                                'first': 'James', 
-                                'last': 'Shore', 
-                                'title': 'The Art of Agile Development'}] )
+    # tests a normal, successful run.
+    def test_normal_use(self):
+        records = load_file('csv', ',', ['title','last','first','year'])
+        self.assertEqual(records, [{'year': '2008', 
+                                    'first': 'Robert', 
+                                    'last': 'Martin', 
+                                    'title': 'Clean Code'}, 
+                                   {'year': '2008', 
+                                    'first': 'James', 
+                                    'last': 'Shore', 
+                                    'title': 'The Art of Agile Development'}] )
 
-  def test_no_file(self):
-    # test wrong file name
-    self.assertRaisesRegexp(IOError, 
-                            "No such file or directory: 'csx'", 
-                            load_file,
-                            'csx', ',', ['title','last','first','year'])
+    def test_no_file(self):
+        # test wrong file name
+        self.assertRaisesRegexp(IOError, 
+                                "No such file or directory: 'csx'", 
+                                load_file,
+                                'csx', ',', ['title','last','first','year'])
 
-  def test_too_few_columns(self):
-    # Too few columns specified for data.
-    # currently, it silently loads the columns specified, and
-    # ignores the extras.
+    def test_too_few_columns(self):
+        # Too few columns specified for data.
+        # currently, it silently loads the columns specified, and
+        # ignores the extras.
 
-    # this is likely a bad behavior, just keeping it simple.
-    records = load_file('csv', ',', ['title','last','first'])
-    self.assertEqual(records, [{'first': 'Robert', 
-                                'last': 'Martin', 
-                                'title': 'Clean Code'}, 
-                               {'first': 'James', 
-                                'last': 'Shore', 
-                                'title': 'The Art of Agile Development'}] )
+        # this is likely a bad behavior, just keeping it simple.
+        records = load_file('csv', ',', ['title','last','first'])
+        self.assertEqual(records, [{'first': 'Robert', 
+                                    'last': 'Martin', 
+                                    'title': 'Clean Code'}, 
+                                   {'first': 'James', 
+                                    'last': 'Shore', 
+                                    'title': 'The Art of Agile Development'}] )
 
-  def test_too_many_columns(self):
-    # too many columns specified for data
-    # same as above, except it ignores the extra specified columns
-    records = load_file('csv', ',', ['title','last','first', 'year', 'bogus'])
-    self.assertEqual(records, [{'year': '2008', 
-                                'first': 'Robert', 
-                                'last': 'Martin', 
-                                'title': 'Clean Code'}, 
-                               {'year': '2008', 
-                                'first': 'James', 
-                                'last': 'Shore', 
-                                'title': 'The Art of Agile Development'}] )
-    
-  def test_filter(self):
-    # a filter that matches one record, but not both
-    records = load_file('csv', ',', ['title','last','first','year'], 'James')
-    self.assertEqual(records, [ {'year': '2008', 
-                                 'first': 'James', 
-                                 'last': 'Shore', 
-                                 'title': 'The Art of Agile Development'}] )
+    def test_too_many_columns(self):
+        # too many columns specified for data
+        # same as above, except it ignores the extra specified columns
+        records = load_file('csv', ',', ['title','last','first', 'year', 'bogus'])
+        self.assertEqual(records, [{'year': '2008', 
+                                    'first': 'Robert', 
+                                    'last': 'Martin', 
+                                    'title': 'Clean Code'}, 
+                                   {'year': '2008', 
+                                    'first': 'James', 
+                                    'last': 'Shore', 
+                                    'title': 'The Art of Agile Development'}] )
+      
+    def test_filter(self):
+        # a filter that matches one record, but not both
+        records = load_file('csv', ',', ['title','last','first','year'], 'James')
+        self.assertEqual(records, [ {'year': '2008', 
+                                     'first': 'James', 
+                                     'last': 'Shore', 
+                                     'title': 'The Art of Agile Development'}] )
 
-    # a filter that will match no records
-    records = load_file('csv', ',', ['title','last','first','year'], 'Beeblebrox')
-    self.assertEqual(records, [] )
-   
-    # a filter that will match all records 
-    records = load_file('csv', ',', ['title','last','first','year'], '2008')
-    self.assertEqual(records, [{'year': '2008', 
-                                'first': 'Robert', 
-                                'last': 'Martin', 
-                                'title': 'Clean Code'}, 
-                               {'year': '2008', 
-                                'first': 'James', 
-                                'last': 'Shore', 
-                                'title': 'The Art of Agile Development'}] )
+        # a filter that will match no records
+        records = load_file('csv', ',', ['title','last','first','year'], 'Beeblebrox')
+        self.assertEqual(records, [] )
+       
+        # a filter that will match all records 
+        records = load_file('csv', ',', ['title','last','first','year'], '2008')
+        self.assertEqual(records, [{'year': '2008', 
+                                    'first': 'Robert', 
+                                    'last': 'Martin', 
+                                    'title': 'Clean Code'}, 
+                                   {'year': '2008', 
+                                    'first': 'James', 
+                                    'last': 'Shore', 
+                                    'title': 'The Art of Agile Development'}] )
     
 if __name__ == '__main__':
   unittest.main()
